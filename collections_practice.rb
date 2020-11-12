a8e7c16c8241f38531309188e8e8075ee3a90a7c
Matt Smith <mrandolph83@gmail.com>
2020-11-12 23:36:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@
 def sort_array_asc(array)
   array.sort {|a, b|a <=> b }
 end
-
+# Comparing Arrays
 def sort_array_desc(array)
   array.sort {|a, b|b <=> a }
 end
@@ -12,6 +12,7 @@ def sort_array_char_count(words)
 end
 
 def swap_elements(array)
+# swaps the second and third elements
   array[1], array[2] = array[2], array[1]
   array
 end
@@ -26,6 +27,8 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
+# takes in an array of strings and replaces the third
+# character in each string with a $
   keshified_words = []
   array.each do |words|
     words_array = words.split("")
@@ -36,14 +39,19 @@ def kesha_maker(array)
 end
 
 def find_a(word_array)
+# returns all the strings in the array passed to it
+# that start_with? (hint) the letter "a"
     word_array.select{|word| word.start_with?("a")}
 end
 
 def sum_array(numbers)
+# adds together all intergers and returns the sum
   numbers.inject(:+)
 end
 
 def add_s(array)
+# adds an "s" to each word in the array except for the
+# second element in the array
   array.each_with_index.collect do |word, index|
     if index == 1
       word
